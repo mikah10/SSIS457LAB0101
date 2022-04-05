66ac914f2b2f914175f9e5b438bb0d601d905f4d
Samantha Mikaela Hinojosa Yucra <102490832+mikah10@users.noreply.github.com>
2022-04-05 10:50:08 -0400

    Add files via upload

---
+++ SSIS457LAB0101/PSIS457LAB0101/principal.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int i; i = 0;
+	int s; s = 1;
+	int v; v = 2;
+	do { 
+		cout << "Hola mi nombre es Samantha Mikaela Hinojosa Yucra usualmente solo mi familia me llama por mi primer nombre. " << endl;
+		cout << "Soy de la ciudad de Sucre naci el 10 de octubre de 2003 y creci en esa ciudad al igual que mis padres,\n";
+		cout << "ellos son profesionales, mi padre trabaja como ingeniero agrónomo y mi madre como maestra ambos me apoyan en mis estudios." << endl;
+		cout << "Soy la hija mayor y tengo dos hermanas, me gustan las mascotas en especial los perros y tengo cuatro perros" << endl;
+		cout << "Estudie la primaria y secundaria en la Unidad educativa Nazareno del cual salí promoción el 2020. " << endl;
+		cout << "Actualmente estoy en el segundo año de la carrera Ingeniería en Sistemas en la universidad San Francisco Javier de Chuquisaca." << endl;
+		cout << "De manera continua estoy estudiando el idioma ingles en el cual ya tengo un buen nivel de fluidez," << endl;
+		cout << "otro idioma que estoy aprendiendo es el frances pero de manera independiente." << endl;
+		cout << "También me interesa bastante el arte digital y lo práctico de manera independiente y me gustaria aprenderlo de forma seria. "<<endl;
+		cout << "Biografia de Samantha Mikaela Hinojosa" << endl;
+		cout << "presione 1 si desea volver a leer" << endl;
+		cout << "presione 2 si desea salir" << endl;
+		cin >> i;
+
+	} 
+	while (i != 2);
+	system("pause");
+}
+
+
+
+	
+
